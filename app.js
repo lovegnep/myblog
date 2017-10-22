4277871b0476cb1d27936f1bd392a18708eed885
gnep <lovegnep@163.com>
2017-10-22 16:26:54 +0800

    add

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var config = require('./config');
+var _ = require('lodash');
+var busboy = require('connect-busboy');
+var bytes = require('bytes');
+var app = express();
+
+var RateLimit = require('express-rate-limit');
+
+app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+
+var limiter = new RateLimit({
+    windowMs: 1*60*1000, // 15 minutes
+    max: 1000, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
+
+//  apply to all requests
+app.use(limiter);
+
+function gettime(d){
+    //if(typeof d !== Date)
+        //return '';
+    return d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+}
+app.locals.gettime=gettime;
+//app.locals.markdown = markdown;
+// view engine setup
+// configuration in all env
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs-mate'));
+//app.engine("html",require("ejs").__express);
+app.locals._layoutFile = 'layout.html';
+_.extend(app.locals, {
+    config: config,
+});
+_.extend(app.locals, require('./render_helper'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser(config.session_secret));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: config.session_secret,
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(busboy({
+    limits: {
+        fileSize: bytes(config.file_limit)
+    }
+}));
+app.use('/',function (req,res,next) {
+   if(req.signedCookies[config.auth_cookie_name] && req.signedCookies[config.auth_cookie_name] === config.auth_cookie_val) {
+       req.session.user='admin';
+   }
+   next();
+});
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
