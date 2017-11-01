12585a351130fdd4794936935fa73df346ab8a54
gnep <lovegnep@163.com>
2017-11-01 22:30:41 +0800

    修改网站图标

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ _.extend(app.locals, {
 });
 _.extend(app.locals, require('./render_helper'));
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon2.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
